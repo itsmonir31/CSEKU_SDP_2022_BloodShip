7410757192f86474b042c21398198f7a3d8b97b2
MONIR <moniruzzaman1928@gmail.com>
2022-02-15 09:31:59 +0600

    Initialization and Splash Screen created

---
+++ app/src/main/java/com/example/bloodship/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.example.bloodship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
